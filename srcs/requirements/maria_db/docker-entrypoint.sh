0d93554a64644b54b02322b45464b8e900000168
Vojtech Parkan <voparkan@c3r4s1.42prague.com>
2025-11-15 16:10:33 +0100

    virtualbox linux problems solving

--- srcs/requirements/maria_db/docker-entrypoint.sh
+++ srcs/requirements/maria_db/docker-entrypoint.sh
@@ -2,6 +2,8 @@
 set -eo pipefail
 shopt -s nullglob
 
+ls -la /var/lib/mysql
+
 # logging functions
 mysql_log() {
 	local type="$1"; shift
@@ -62,38 +64,38 @@ _is_sourced() {
 # usage: docker_process_init_files [file [file [...]]]
 #    ie: docker_process_init_files /always-initdb.d/*
 # process initializer files, based on file extensions
-docker_process_init_files() {
-	# mysql here for backwards compatibility "${mysql[@]}"
-	# ShellCheck: mysql appears unused. Verify use (or export if used externally)
-	# shellcheck disable=SC2034
-	mysql=( docker_process_sql )
-
-	echo
-	local f
-	for f; do
-		case "$f" in
-			*.sh)
-				# https://github.com/docker-library/postgres/issues/450#issuecomment-393167936
-				# https://github.com/docker-library/postgres/pull/452
-				if [ -x "$f" ]; then
-					mysql_note "$0: running $f"
-					"$f"
-				else
-					mysql_note "$0: sourcing $f"
-					# ShellCheck can't follow non-constant source. Use a directive to specify location.
-					# shellcheck disable=SC1090
-					. "$f"
-				fi
-				;;
-			*.sql)     mysql_note "$0: running $f"; docker_process_sql < "$f"; echo ;;
-			*.sql.gz)  mysql_note "$0: running $f"; gunzip -c "$f" | docker_process_sql; echo ;;
-			*.sql.xz)  mysql_note "$0: running $f"; xzcat "$f" | docker_process_sql; echo ;;
-			*.sql.zst) mysql_note "$0: running $f"; zstd -dc "$f" | docker_process_sql; echo ;;
-			*)         mysql_warn "$0: ignoring $f" ;;
-		esac
-		echo
-	done
-}
+#docker_process_init_files() {
+#	# mysql here for backwards compatibility "${mysql[@]}"
+#	# ShellCheck: mysql appears unused. Verify use (or export if used externally)
+#	# shellcheck disable=SC2034
+#	mysql=( docker_process_sql )
+#
+#	echo
+#	local f
+#	for f; do
+#		case "$f" in
+#			*.sh)
+#				# https://github.com/docker-library/postgres/issues/450#issuecomment-393167936
+#				# https://github.com/docker-library/postgres/pull/452
+#				if [ -x "$f" ]; then
+#					mysql_note "$0: running $f"
+#					"$f"
+#				else
+#					mysql_note "$0: sourcing $f"
+#					# ShellCheck can't follow non-constant source. Use a directive to specify location.
+#					# shellcheck disable=SC1090
+#					. "$f"
+#				fi
+#				;;
+#			*.sql)     mysql_note "$0: running $f"; docker_process_sql < "$f"; echo ;;
+#			*.sql.gz)  mysql_note "$0: running $f"; gunzip -c "$f" | docker_process_sql; echo ;;
+#			*.sql.xz)  mysql_note "$0: running $f"; xzcat "$f" | docker_process_sql; echo ;;
+#			*.sql.zst) mysql_note "$0: running $f"; zstd -dc "$f" | docker_process_sql; echo ;;
+#			*)         mysql_warn "$0: ignoring $f" ;;
+#		esac
+#		echo
+#	done
+#}
 
 # arguments necessary to run "mariadbd --verbose --help" successfully (used for testing configuration validity and for extracting default/configured values)
 _verboseHelpArgs=(
@@ -202,6 +204,8 @@ docker_create_db_directories() {
 	# TODO other directories that are used by default? like /var/lib/mysql-files
 	# see https://github.com/docker-library/mysql/issues/562
 	mkdir -p "$DATADIR"
+	chown -R mysql:mysql "$DATADIR"
+	chown -R mysql:mysql /var/lib/mysql
 
 	if [ "$user" = "0" ]; then
 		# this will cause less disk access than `chown -R`
@@ -210,7 +214,6 @@ docker_create_db_directories() {
 		if [ "${SOCKET:0:1}" != '@' ]; then # not abstract sockets
 			find "${SOCKET%/*}" -maxdepth 0 \! -user mysql \( -exec chown mysql: '{}' \; -o -true \)
 		fi
-
 		# memory.pressure
 		local cgroup; cgroup=$(</proc/self/cgroup)
 		local mempressure="/sys/fs/cgroup/${cgroup:3}/memory.pressure"
@@ -229,6 +232,9 @@ _mariadb_version() {
 # initializes the database directory
 docker_init_database_dir() {
 	mysql_note "Initializing database files"
+	chmod -R a+rwx /var/lib/mysql
+  LS=$(ls -la /var/lib)
+  mysql_warn "$LS"
 	installArgs=( --datadir="$DATADIR" --rpm --auth-root-authentication-method=normal )
 	# "Other options are passed to mariadbd." (so we pass all "mariadbd" arguments directly here)
 
@@ -396,6 +402,10 @@ docker_setup_db() {
 				CREATE USER 'root'@'${MARIADB_ROOT_HOST}' IDENTIFIED BY PASSWORD '${MARIADB_ROOT_PASSWORD_HASH}' ;
 				GRANT ALL ON *.* TO 'root'@'${MARIADB_ROOT_HOST}' WITH GRANT OPTION ;
 				GRANT PROXY ON ''@'%' TO 'root'@'${MARIADB_ROOT_HOST}' WITH GRANT OPTION;
+        CREATE DATABASE IF NOT EXISTS $DB_NAME ;
+        CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_PWD' ;
+        GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'%' ;
+        FLUSH PRIVILEGES;
 			EOSQL
 		else
 			read -r -d '' rootCreate <<-EOSQL || true
@@ -546,7 +556,7 @@ docker_mariadb_init()
 	mysql_note "Temporary server started."
 
 	docker_setup_db
-	docker_process_init_files /docker-entrypoint-initdb.d/*
+#	docker_process_init_files /docker-entrypoint-initdb.d/*
 	# Wait until after /docker-entrypoint-initdb.d is performed before setting
 	# root@localhost password to a hash we don't know the password for.
 	if [ -n "${MARIADB_ROOT_PASSWORD_HASH}" ]; then
@@ -603,33 +613,8 @@ docker_mariadb_upgrade() {
 		--loose-innodb_buffer_pool_dump_at_shutdown=0
 	mysql_note "Temporary server started."
 
-	docker_mariadb_backup_system
+#	docker_mariadb_backup_system
 
-	if [ ! -f "$DATADIR"/.my-healthcheck.cnf ]; then
-		mysql_note "Creating healthcheck users"
-		local createHealthCheckUsers
-		createHealthCheckUsers=$(create_healthcheck_users)
-		docker_process_sql --dont-use-mysql-root-password --binary-mode <<-EOSQL
-		-- Healthcheck users shouldn't be replicated
-		SET @@SESSION.SQL_LOG_BIN=0;
-                -- we need the SQL_MODE NO_BACKSLASH_ESCAPES mode to be clear for the password to be set
-		SET @@SESSION.SQL_MODE=REPLACE(@@SESSION.SQL_MODE, 'NO_BACKSLASH_ESCAPES', '');
-		FLUSH PRIVILEGES;
-		$createHealthCheckUsers
-EOSQL
-		mysql_note "Stopping temporary server"
-		docker_temp_server_stop
-		mysql_note "Temporary server stopped"
-
-		if _check_if_upgrade_is_needed; then
-			# need a restart as FLUSH PRIVILEGES isn't reversable
-			mysql_note "Restarting temporary server for upgrade"
-			docker_temp_server_start "$@" --skip-grant-tables \
-				--loose-innodb_buffer_pool_dump_at_shutdown=0
-		else
-			return 0
-		fi
-	fi
 
 	mysql_note "Starting mariadb-upgrade"
 	mariadb-upgrade --upgrade-system-tables
--- srcs/requirements/wp/docker-entrypoint.sh
+++ srcs/requirements/wp/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-gosu www-data wp core download --path=/var/www/html --locale=cs_CZ
-gosu www-data wp config create --path=/var/www/html --dbname=$DB_NAME --dbuser=$DB_USR --dbpass=$DB_PWD --dbhost=$DB_SRV --force --allow-root
-gosu www-data wp core install --path=/var/www/html --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PWD --admin_email=$ADMIN_EMAIL --skip-email --allow-root
-gosu www-data wp user create --path=/var/www/html visitor visotor@42.fr --role=subscriber --user_pass=password
+wp core download --path=/var/www/public_html --locale=cs_CZ --allow-root
+wp config create --path=/var/www/public_html --dbname=$DB_NAME --dbuser=$DB_USR --dbpass=$DB_PWD --dbhost=$DB_SRV --force --allow-root
+wp core install --path=/var/www/public_html --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PWD --admin_email=$ADMIN_EMAIL --skip-email --allow-root
+wp user create --path=/var/www/public_html visitor visotor@42.fr --role=subscriber --user_pass=$USER_PWD --allow-root
 
 /usr/sbin/php-fpm8.3 -F
